a129e5346612be6e04b013caf7fb2487ac78be94
TAKUMI <tkm79_nazario@outlook.jp>
2023-06-01 16:55:31 +0900

    各URLを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'home#index'
+  get '/about' => 'home#about'
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
+# 端的に言うと、個々の部分でURLを作るところ
+# rootはよく使う
