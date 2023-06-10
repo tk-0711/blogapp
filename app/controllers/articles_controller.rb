522ab8232d3ebdc900c9b847a3206aa17991ecea
TAKUMI <tkm79_nazario@outlook.jp>
2023-06-10 20:25:25 +0900

    createアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,4 +16,17 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to article_path(@article)
+        else
+            render :new
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 end
