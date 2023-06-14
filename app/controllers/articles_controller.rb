5100f948415d0f687772dd841b93769b18af6fe9
TAKUMI <tkm79_nazario@outlook.jp>
2023-06-14 16:25:21 +0900

    before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,15 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+
     # def index
     #     render 'home/index' # <-- は省略可
     # end
 
     def index
-        @title = 'デイトラ'
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -27,11 +27,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新できました'
         else
@@ -41,13 +39,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
-        @article.destroy!
+        article = Article.find(params[:id])
+        article.destroy!
         redirect_to root_path, notice: '削除に成功しました'
       end
 
     private
     def article_params
         params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
