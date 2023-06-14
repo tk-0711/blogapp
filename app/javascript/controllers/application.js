5847261fbb48870f8ef9ab33d6f23a64b329b7f7
TAKUMI <tkm79_nazario@outlook.jp>
2023-06-14 15:57:40 +0900

    bootstrapとjavascriptを作動させるための変更

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,3 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/turbo-rails"
 
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+window.Turbo = Turbo
