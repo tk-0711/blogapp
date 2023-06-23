8cdc4fdfb2c280a902d1d970344f750f6eb8eca8
TAKUMI <tkm79_nazario@outlook.jp>
2023-06-23 14:36:26 +0900

    DBのスキーマを修正

--- db/migrate/20230601081048_create_articles.rb
+++ db/migrate/20230601081048_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title # stringは、短い文字列
-      t.text :content # textは、長い文字列
+      t.references :user, null: false
+      t.string :title, null: false # stringは、短い文字列
+      t.text :content, null: false # textは、長い文字列
       t.timestamps
     end
   end
